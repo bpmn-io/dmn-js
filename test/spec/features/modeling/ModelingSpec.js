aa524ec5400d47d482cbfbd4aa06c9bb33cac52e
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-07-26 16:41:34 +0200

    feat(expression-language): allow setting expression language per cell
    Closes #20

---
+++ test/spec/features/modeling/ModelingSpec.js
@@ -0,0 +1,64 @@
+'use strict';
+
+var TestHelper = require('../../../TestHelper');
+
+/* global bootstrapModeler, inject */
+
+
+describe('features/modeling', function() {
+
+  var modeler;
+
+  beforeEach(function(done) {
+    modeler = bootstrapModeler({}, require('fs').readFileSync(__dirname + '/../../../fixtures/dmn/boolean.dmn', 'utf-8'))(done);
+  });
+
+  describe('cellExpressionLanguage', function() {
+    var businessObject;
+
+    beforeEach(inject(function(elementRegistry) {
+      businessObject = elementRegistry.get('cell_input1_rule1').content;
+    }));
+    it('should set the expression language for a cell', inject(function(modeling) {
+      // when
+      modeling.editCellExpressionLanguage(businessObject, 'myScriptLanguage');
+
+      // then
+      expect(businessObject.expressionLanguage).to.eql('myScriptLanguage');
+    }));
+
+    it('should undo', inject(function(modeling, commandStack) {
+      // given
+      modeling.editCellExpressionLanguage(businessObject, 'myScriptLanguage');
+
+      // when
+      commandStack.undo();
+
+      // then
+      expect(businessObject.expressionLanguage).to.not.exist;
+    }));
+
+    it('should redo', inject(function(modeling, commandStack) {
+      // given
+      modeling.editCellExpressionLanguage(businessObject, 'myScriptLanguage');
+      commandStack.undo();
+
+      // when
+      commandStack.redo();
+
+      // then
+      expect(businessObject.expressionLanguage).to.eql('myScriptLanguage');
+    }));
+
+    it('should persist the change in the xml', inject(function(modeling) {
+      // given
+      modeling.editCellExpressionLanguage(businessObject, 'myScriptLanguage');
+
+      // when
+      modeler.saveXML(function(err, xml) {
+        expect(xml).to.include('expressionLanguage="myScriptLanguage"');
+      });
+    }));
+  });
+
+});
