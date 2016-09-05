c9043aac0a98b999979afd5ddc73fd10e5143384
Ricardo Matias <ricardo.matias@camunda.com>
2016-09-05 15:43:31 +0200

    feat(descriptions): don't add a description when it's an enter char
    Closes #57

--- test/spec/features/descriptions/DescriptionsSpec.js
+++ test/spec/features/descriptions/DescriptionsSpec.js
@@ -2,11 +2,12 @@
 
 require('../../../TestHelper');
 
-/* global bootstrapModeler, inject */
+/* global bootstrapModeler, inject, injectAsync */
 
 var EventUtils = require('../../../util/EventUtils'),
     queryElement = require('../../../util/ElementUtils').queryElement,
     rightClickElement = EventUtils.rightClickElement,
+    inputEvent = EventUtils.inputEvent,
     clickElement = EventUtils.clickElement;
 
 var basicXML = require('../../../fixtures/dmn/simple.dmn');
@@ -15,7 +16,7 @@ describe('features/descriptions', function() {
 
   beforeEach(bootstrapModeler(basicXML));
 
-  it('should add a description', inject(function(elementRegistry, eventBus) {
+  it('should create a textarea when executing: add description', inject(function(elementRegistry, eventBus) {
 
     var rule1 = elementRegistry.get('cell_input1_rule1'),
         contextmenu;
@@ -32,6 +33,39 @@ describe('features/descriptions', function() {
 
     // when
     clickElement(queryElement('[data-id="description"]', contextmenu));
+  }));
+
+
+  it('should add a description', injectAsync(function(done) {
+    return function(elementRegistry, eventBus, sheet) {
+
+      var rule1 = elementRegistry.get('cell_input1_rule1'),
+          container = sheet.getContainer(),
+          contextmenu, textarea;
+
+      // when - open context-menu
+      rightClickElement(rule1);
+
+      contextmenu = queryElement('.tjs-context-menu');
+
+      // when
+      clickElement(queryElement('[data-id="description"]', contextmenu));
+
+      textarea = queryElement('.descriptions-textarea');
+
+      // when
+      inputEvent(textarea, 'hello');
+
+      eventBus.on('commandStack.description.edit.postExecute', function() {
+
+        // then
+        expect(rule1.content.description).to.equal('hello');
+
+        done();
+      });
+
+      clickElement(container);
+    };
   }));
 
 });
