d0125a18c6ef8446a16c895dd56bae5de0b25a2c
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-09-08 13:55:35 +0200

    chore(string-edit): apply transient input on editor close
    test(input-values): add test for allowed values modeling methods
    fix(string-edit): hide string-content when switching datatype
    
    related to #27

--- test/spec/features/modeling/ModelingSpec.js
+++ test/spec/features/modeling/ModelingSpec.js
@@ -120,4 +120,111 @@ describe('features/modeling', function() {
 
   });
 
+  describe('input values', function() {
+    describe('add', function() {
+      var businessObject;
+
+      beforeEach(inject(function(elementRegistry) {
+        businessObject = elementRegistry.get('cell_input1_typeRow');
+      }));
+
+      it('should set input values for a column', inject(function(modeling) {
+        // when
+        modeling.addAllowedValue(businessObject, 'inputValue');
+
+        // then
+        expect(businessObject.content.inputValues.text).to.include('inputValue');
+      }));
+
+
+      it('should undo', inject(function(modeling, commandStack) {
+        // given
+        modeling.addAllowedValue(businessObject, 'inputValue');
+
+        // when
+        commandStack.undo();
+
+        // then
+        expect(businessObject.content.inputValues.text).to.be.empty;
+      }));
+
+
+      it('should redo', inject(function(modeling, commandStack) {
+        // given
+        modeling.addAllowedValue(businessObject, 'inputValue');
+        commandStack.undo();
+
+        // when
+        commandStack.redo();
+
+        // then
+        expect(businessObject.content.inputValues.text).to.include('inputValue');
+      }));
+
+
+      it('should persist the change in the xml', inject(function(modeling) {
+        // given
+        modeling.addAllowedValue(businessObject, 'inputValue');
+
+        // when
+        modeler.saveXML(function(err, xml) {
+          expect(xml).to.include('<dmn:text><![CDATA["inputValue"]]></dmn:text>');
+        });
+      }));
+    });
+
+    describe('remove', function() {
+      var businessObject;
+
+      beforeEach(inject(function(elementRegistry, modeling) {
+        businessObject = elementRegistry.get('cell_input1_typeRow');
+        modeling.addAllowedValue(businessObject, 'inputValue');
+      }));
+
+      it('should remove input values for a column', inject(function(modeling) {
+        // when
+        modeling.removeAllowedValue(businessObject, 'inputValue');
+
+        // then
+        expect(businessObject.content.inputValues.text).to.be.empty;
+      }));
+
+
+      it('should undo', inject(function(modeling, commandStack) {
+        // given
+        modeling.removeAllowedValue(businessObject, 'inputValue');
+
+        // when
+        commandStack.undo();
+
+        // then
+        expect(businessObject.content.inputValues.text).to.include('inputValue');
+      }));
+
+
+      it('should redo', inject(function(modeling, commandStack) {
+        // given
+        modeling.removeAllowedValue(businessObject, 'inputValue');
+        commandStack.undo();
+
+        // when
+        commandStack.redo();
+
+        // then
+        expect(businessObject.content.inputValues.text).to.be.empty;
+      }));
+
+
+      it('should persist the change in the xml', inject(function(modeling) {
+        // given
+        modeling.removeAllowedValue(businessObject, 'inputValue');
+
+        // when
+        modeler.saveXML(function(err, xml) {
+          expect(xml).to.include('<dmn:text></dmn:text>');
+        });
+      }));
+    });
+  });
+
 });
