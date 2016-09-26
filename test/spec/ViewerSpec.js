bff31fec23bc8ef148b6731a830d20187015ffed
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-09-26 11:10:40 +0200

    feat(drd): add drd rendering
    Closes #79
    Related #96

--- test/spec/ViewerSpec.js
+++ test/spec/ViewerSpec.js
@@ -1,12 +1,9 @@
 'use strict';
 
-var Viewer = require('../../lib/Viewer');
+var Viewer = require('../../lib/TableViewer');
 
-var simpleXML = require('../fixtures/dmn/simple.dmn'),
-    emptyDefsXML = require('../fixtures/dmn/empty-definitions.dmn'),
-    emptyDecisionXML = require('../fixtures/dmn/empty-decision-id.dmn'),
-    noDecisionXML = require('../fixtures/dmn/no-decision-id.dmn'),
-    multipleTablesXML = require('../fixtures/dmn/multiple-tables.dmn');
+var exampleXML = require('../fixtures/dmn/di.dmn'),
+    emptyDefsXML = require('../fixtures/dmn/empty-definitions.dmn');
 
 var TestContainer = require('mocha-test-container-support');
 
@@ -29,7 +26,7 @@ describe('Viewer', function() {
 
 
   it('should import simple process', function(done) {
-    createViewer(simpleXML, done);
+    createViewer(exampleXML, done);
   });
 
 
@@ -38,29 +35,9 @@ describe('Viewer', function() {
   });
 
 
-  it('should import missing id on decision', function(done) {
-    createViewer(noDecisionXML, function(err, warnings, viewer) {
-      expect(viewer.definitions.decision[0].id).to.eql(undefined);
-      done();
-    });
-  });
-
-
-  it('should repair empty id on decision', function(done) {
-    createViewer(emptyDecisionXML, function(err, warnings, viewer) {
-      expect(err).to.not.exist;
-      expect(warnings).to.have.length(0);
-
-      expect(viewer.definitions.decision[0].id).to.not.eql(undefined);
-      expect(viewer.definitions.decision[0].id).to.not.eql('');
-      done();
-    });
-  });
-
-
   it('should re-import simple process', function(done) {
     // given
-    createViewer(simpleXML, function(err, warnings, viewer) {
+    createViewer(exampleXML, function(err, warnings, viewer) {
 
       if (err) {
         return done(err);
@@ -68,7 +45,7 @@ describe('Viewer', function() {
 
       // when
       // mimic re-import of same diagram
-      viewer.importXML(simpleXML, function(err, warnings) {
+      viewer.importXML(exampleXML, function(err, warnings) {
 
         // then
         expect(err).to.not.exist;
@@ -88,7 +65,7 @@ describe('Viewer', function() {
 
       var viewer = new Viewer();
 
-      viewer.importXML(simpleXML, function(err, warnings) {
+      viewer.importXML(exampleXML, function(err, warnings) {
 
         expect(viewer.container.parentNode).to.eql(document.body);
 
@@ -125,7 +102,7 @@ describe('Viewer', function() {
       });
 
       // when
-      viewer.importXML(emptyDefsXML, function(err) {
+      viewer.importXML(exampleXML, function(err) {
 
         // then
         expect(events).to.eql([
@@ -140,50 +117,6 @@ describe('Viewer', function() {
       });
     });
 
-  });
-
-
-  describe('xml with multiple tables', function() {
-    it('should display the first table by default', function(done) {
-      // given
-      var viewer = new Viewer();
-
-      // when
-      viewer.importXML(multipleTablesXML, function(err, warnings) {
-
-        // then
-        expect(viewer.container.querySelector('header > h3').textContent).to.eql('Dish Decision');
-        done();
-      });
-    });
-
-    it('should expose all contained decision tables', function(done) {
-      // given
-      var viewer = new Viewer();
-
-      // when
-      viewer.importXML(multipleTablesXML, function(err, warnings) {
-
-        // then
-        expect(viewer.getDecisions().length).to.eql(3);
-        done();
-      });
-    });
-
-    it('should switch between decision tables', function(done) {
-      // given
-      var viewer = new Viewer();
-
-      viewer.importXML(multipleTablesXML, function(err, warnings) {
-
-        // when
-        viewer.showDecision(viewer.getDecisions()[2]);
-
-        // then
-        expect(viewer.container.querySelector('header > h3').textContent).to.eql('Guest Count');
-        done();
-      });
-    });
   });
 
 });
