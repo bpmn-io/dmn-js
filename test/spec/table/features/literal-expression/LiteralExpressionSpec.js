c145c70bcb7def5395f688a970700ce8c2589f91
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-10-13 15:00:19 +0200

    feat(lit-exp): add literal expression editor
    Closes #107

---
+++ test/spec/table/features/literal-expression/LiteralExpressionSpec.js
@@ -0,0 +1,85 @@
+'use strict';
+
+require('../../TestHelper');
+
+/* global bootstrapModeler, inject */
+
+var basicXML = require('../../../../fixtures/dmn/literal-expression.dmn');
+
+describe('features/literal-expression', function() {
+  var modeler;
+
+  beforeEach(function(done) {
+    modeler = bootstrapModeler(basicXML)(done);
+  });
+
+  it('should should show the literal expression editor', inject(function(sheet) {
+    expect(sheet.getContainer().querySelector('.literal-expression-editor')).to.exist;
+  }));
+
+  it('should render the literal expression text', inject(function(sheet) {
+    expect(sheet.getContainer().querySelector('textarea').value).to.eql('calendar.getSeason(date)');
+  }));
+
+  it('should render variable name and type', inject(function(sheet) {
+    expect(sheet.getContainer().querySelector('.variable-name').value).to.eql('season');
+    expect(sheet.getContainer().querySelector('.variable-type input').value).to.eql('string');
+  }));
+
+  it('should render expression language', inject(function(sheet) {
+    expect(sheet.getContainer().querySelector('.expression-language input').value).to.eql('Javascript');
+  }));
+
+  describe('modeling', function() {
+    var businessObject;
+
+    beforeEach(inject(function(elementRegistry) {
+      businessObject = modeler.getDecisions()[0];
+    }));
+
+    it('should set the literal expression', inject(function(modeling) {
+      // when
+      modeling.editLiteralExpression(businessObject, 'myNewLiteralExpression', 'varName', 'string', 'Javascript');
+
+      // then
+      expect(businessObject.literalExpression.text).to.eql('myNewLiteralExpression');
+    }));
+
+
+    it('should undo', inject(function(modeling, commandStack) {
+      // given
+      modeling.editLiteralExpression(businessObject, 'myNewLiteralExpression', 'varName', 'string', 'Javascript');
+
+      // when
+      commandStack.undo();
+
+      // then
+      expect(businessObject.literalExpression.text).to.eql('calendar.getSeason(date)');
+    }));
+
+
+    it('should redo', inject(function(modeling, commandStack) {
+      // given
+      modeling.editLiteralExpression(businessObject, 'myNewLiteralExpression', 'varName', 'string', 'Javascript');
+      commandStack.undo();
+
+      // when
+      commandStack.redo();
+
+      // then
+      expect(businessObject.literalExpression.text).to.eql('myNewLiteralExpression');
+    }));
+
+
+    it('should persist the change in the xml', inject(function(modeling) {
+      // given
+      modeling.editLiteralExpression(businessObject, 'myNewLiteralExpression', 'varName', 'string', 'Javascript');
+
+      // when
+      modeler.saveXML(function(err, xml) {
+        expect(xml).to.include('<text>myNewLiteralExpression</text>');
+      });
+    }));
+  });
+
+});
