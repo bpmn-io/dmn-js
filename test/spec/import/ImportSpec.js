954895296b339c5df57b5cd42f627d3d2e625cba
Ricardo Matias <ricardo.matias@camunda.com>
2016-10-13 15:15:48 +0200

    fix(import): correctly import and link requirements

--- test/spec/import/ImportSpec.js
+++ test/spec/import/ImportSpec.js
@@ -6,24 +6,40 @@ var exampleXML = require('../../fixtures/dmn/di.dmn');
 
 var TestContainer = require('mocha-test-container-support');
 
+
+function expectConnection(connection, type) {
+  var businessObject = connection.businessObject;
+
+  expect(connection.type).to.equal(type);
+  expect(connection.waypoints).to.have.length.of(2);
+
+  expect(businessObject.$type).to.equal(type);
+  expect(businessObject.di.$type).to.equal('biodi:Edge');
+  expect(businessObject.di.waypoints).to.have.length.of(2);
+}
+
 describe('DRD - Import', function() {
 
   var viewer, elementRegistry;
 
   var getElementWithSourceAndTargetType = function(sourceType, targetType) {
     var match;
+
     elementRegistry.forEach(function(el) {
       if (el.source && el.source.type === sourceType &&
          el.target && el.target.type === targetType) {
         match = el;
       }
     });
+
     return match;
   };
 
-  beforeEach(function(done) {
-    TestContainer.get(this);
-    viewer = new Viewer();
+  before(function(done) {
+    var container = TestContainer.get(this);
+
+    viewer = new Viewer({ container: container });
+
     viewer.importXML(exampleXML, function() {
       elementRegistry = viewer.get('elementRegistry');
       done();
@@ -31,21 +47,39 @@ describe('DRD - Import', function() {
   });
 
   describe('connection types', function() {
+
     it('should connect decisions with information requirement', function() {
-      var conn = getElementWithSourceAndTargetType('dmn:Decision', 'dmn:Decision');
-      expect(conn.type).to.eql('dmn:InformationRequirement');
+      var connection = getElementWithSourceAndTargetType('dmn:Decision', 'dmn:Decision');
+
+      expectConnection(connection, 'dmn:InformationRequirement');
     });
+
+
     it('should connect business knowledge model to decisions with knowledge requirement', function() {
-      var conn = getElementWithSourceAndTargetType('dmn:BusinessKnowledgeModel', 'dmn:Decision');
-      expect(conn.type).to.eql('dmn:KnowledgeRequirement');
+      var connection = getElementWithSourceAndTargetType('dmn:BusinessKnowledgeModel', 'dmn:Decision');
+
+      expectConnection(connection, 'dmn:KnowledgeRequirement');
     });
+
+
     it('should connect knowledge source to decisions with authority requirement', function() {
-      var conn = getElementWithSourceAndTargetType('dmn:KnowledgeSource', 'dmn:Decision');
-      expect(conn.type).to.eql('dmn:AuthorityRequirement');
+      var connection = getElementWithSourceAndTargetType('dmn:KnowledgeSource', 'dmn:Decision');
+
+      expectConnection(connection, 'dmn:AuthorityRequirement');
     });
+
+
     it('should connect input data to decisions with information requirement', function() {
-      var conn = getElementWithSourceAndTargetType('dmn:InputData', 'dmn:Decision');
-      expect(conn.type).to.eql('dmn:InformationRequirement');
+      var connection = getElementWithSourceAndTargetType('dmn:InputData', 'dmn:Decision');
+
+      expectConnection(connection, 'dmn:InformationRequirement');
+    });
+
+
+    it('should connect input data to text annotation with an association', function() {
+      var connection = getElementWithSourceAndTargetType('dmn:InputData', 'dmn:TextAnnotation');
+
+      expectConnection(connection, 'dmn:Association');
     });
 
   });
