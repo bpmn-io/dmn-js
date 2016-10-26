96390db2ac5057f08808363e00f5a8fe159d3c58
Ricardo Matias <ricardo.matias@camunda.com>
2016-10-26 15:06:17 +0200

    feat(palette): add a palette provider
    Closes #113

---
+++ test/spec/features/palette/PaletteProviderSpec.js
@@ -0,0 +1,33 @@
+'use strict';
+
+require('../../TestHelper');
+
+/* global bootstrapModeler, inject */
+
+var modelingModule = require('../../../../lib/features/modeling'),
+    paletteModule = require('../../../../lib/features/palette'),
+    coreModule = require('../../../../lib/core');
+
+var domQuery = require('min-dom/lib/query');
+
+
+describe('features/palette', function() {
+
+  var diagramXML = require('../../../fixtures/dmn/input-data.dmn');
+
+  var testModules = [ coreModule, modelingModule, paletteModule ];
+
+  beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
+
+
+  it('should provide DMN modeling palette', inject(function(canvas, palette) {
+
+    // when
+    var paletteElement = domQuery('.djs-palette', canvas._container);
+    var entries = domQuery.all('.entry', paletteElement);
+
+    // then
+    expect(entries.length).to.equal(5);
+  }));
+
+});
