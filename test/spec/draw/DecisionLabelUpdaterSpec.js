7efa39f95d1a0da3b664b980e7176302829a2618
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-11-03 11:52:53 +0100

    fix(decisions): update decision labels after switching to drd view
    Closes #145

---
+++ test/spec/draw/DecisionLabelUpdaterSpec.js
@@ -0,0 +1,30 @@
+'use strict';
+
+require('../TestHelper');
+
+/* global bootstrapModeler, inject */
+
+var coreModule = require('../../../lib/core');
+
+describe('draw/DecisionLabelUpdater', function() {
+
+  var diagramXML = require('../../fixtures/dmn/di.dmn');
+
+  var testModules = [ coreModule ];
+
+  beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
+
+
+  it('should update the Decision Label', inject(function(eventBus, canvas, elementRegistry) {
+
+    // given
+    elementRegistry.get('dish-decision').businessObject.name = 'FOO BAR';
+
+    // when
+    eventBus.fire('view.switch');
+
+    // then
+    expect(elementRegistry.getGraphics('dish-decision').node.textContent).to.eql('FOO BAR');
+  }));
+
+});
