de14fcf25ab1157672b51bd42aa30636d17fd872
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-11-10 11:55:55 +0100

    feat(definition-id): show and allow changing definition id and name
    Closes #149

---
+++ test/spec/features/definition-id/DefinitionIdSpec.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var domQuery = require('min-dom/lib/query');
+
+require('../../TestHelper');
+
+/* global bootstrapModeler, inject */
+
+var modelingModule = require('../../../../lib/features/modeling'),
+    definitionIdViewerModule = require('../../../../lib/features/definition-id/viewer'),
+    definitionIdModelerModule = require('../../../../lib/features/definition-id/modeler'),
+    coreModule = require('../../../../lib/core');
+
+describe('features/definition-id', function() {
+
+  var testModules = [
+    coreModule,
+    definitionIdViewerModule,
+    definitionIdModelerModule,
+    modelingModule
+  ];
+
+  var diagramXML = require('./definitionId.dmn');
+
+  beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
+
+  it('should display the definitions name', inject(function(definitionIdView) {
+    // given
+    var nameContainer = domQuery('.dmn-definitions-name', definitionIdView._container);
+
+    // when
+
+    // then
+    expect(nameContainer.textContent).to.eql('drd-name');
+  }));
+
+
+  it('should display the definitions id', inject(function(definitionIdView) {
+    // given
+    var nameContainer = domQuery('.dmn-definitions-id', definitionIdView._container);
+
+    // when
+
+    // then
+    expect(nameContainer.textContent).to.eql('drd-id');
+  }));
+
+
+  it('should apply changes from updated definitions', inject(function(definitionIdView, canvas) {
+    // given
+    var definitions = canvas.getRootElement().businessObject;
+    var nameContainer = domQuery('.dmn-definitions-name', definitionIdView._container);
+
+    // when
+    definitions.name = 'new Name';
+    definitionIdView.update();
+
+    // then
+    expect(nameContainer.textContent).to.eql('new Name');
+  }));
+
+
+  it('should react to definition name updates', inject(function(definitionIdView, definitionIdEdit) {
+    // given
+    var nameContainer = domQuery('.dmn-definitions-name', definitionIdView._container);
+
+    // when
+    definitionIdEdit.update('name', 'new Name');
+
+    // then
+    expect(nameContainer.textContent).to.eql('new Name');
+  }));
+
+
+  it('should undo', inject(function(definitionIdView, definitionIdEdit, commandStack) {
+    // given
+    var nameContainer = domQuery('.dmn-definitions-name', definitionIdView._container);
+    definitionIdEdit.update('name', 'new Name');
+
+    // when
+    commandStack.undo();
+
+    // then
+    expect(nameContainer.textContent).to.eql('drd-name');
+  }));
+
+
+  it('should redo', inject(function(definitionIdView, definitionIdEdit, commandStack) {
+    // given
+    var nameContainer = domQuery('.dmn-definitions-name', definitionIdView._container);
+    definitionIdEdit.update('name', 'new Name');
+
+    // when
+    commandStack.undo();
+    commandStack.redo();
+
+    // then
+    expect(nameContainer.textContent).to.eql('new Name');
+  }));
+
+});
