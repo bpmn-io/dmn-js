6a10c5d749f35c895c47b253afeba276e240ba96
Sebastian Stamm <sebastian.stamm@camunda.com>
2016-11-29 13:50:33 +0100

    fix(drd-interaction): disable drill-down by config parameter
    Closes #161

--- test/spec/features/drill-down/DrillDownSpec.js
+++ test/spec/features/drill-down/DrillDownSpec.js
@@ -24,70 +24,98 @@ describe('features - drill-down', function() {
 
   var diagramXML = require('../../../fixtures/dmn/multiple-decisions.dmn');
 
-  beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
+  describe('default behavior', function() {
+
+    beforeEach(bootstrapModeler(diagramXML, { modules: testModules }));
 
 
-  it('should add drill down overlays', inject(function(elementRegistry) {
+    it('should add drill down overlays', inject(function(elementRegistry) {
 
-    // given
-    var elements = queryElement('.djs-overlays', true);
-
-    // then
-    expect(elements).to.have.length(3);
-  }));
-
-
-  it('should drill down to a decision table', inject(function(elementRegistry, eventBus) {
-
-    // given
-    var decisionElement = elementRegistry.get('dish-decision'),
-        drillDownOverlay = queryElement('[data-container-id="dish-decision"] div div');
-
-    eventBus.on('decision.open', function(context) {
-      var decision = context.decision;
+      // given
+      var elements = queryElement('.djs-overlays', true);
 
       // then
-      expect(decision).to.equal(decisionElement.businessObject);
-      expect(decision.decisionTable).to.exist;
-    });
-
-    // when
-    mouseEvent('click', drillDownOverlay);
-  }));
+      expect(elements).to.have.length(3);
+    }));
 
 
-  it('should drill down to a literal expression', inject(function(elementRegistry, eventBus) {
+    it('should drill down to a decision table', inject(function(elementRegistry, eventBus) {
 
-    // given
-    var decisionElement = elementRegistry.get('season'),
-        drillDownOverlay = queryElement('[data-container-id="season"] div div');
+      // given
+      var decisionElement = elementRegistry.get('dish-decision'),
+          drillDownOverlay = queryElement('[data-container-id="dish-decision"] div div');
 
-    eventBus.on('decision.open', function(context) {
-      var decision = context.decision;
+      eventBus.on('decision.open', function(context) {
+        var decision = context.decision;
+
+        // then
+        expect(decision).to.equal(decisionElement.businessObject);
+        expect(decision.decisionTable).to.exist;
+      });
+
+      // when
+      mouseEvent('click', drillDownOverlay);
+    }));
+
+
+    it('should drill down to a literal expression', inject(function(elementRegistry, eventBus) {
+
+      // given
+      var decisionElement = elementRegistry.get('season'),
+          drillDownOverlay = queryElement('[data-container-id="season"] div div');
+
+      eventBus.on('decision.open', function(context) {
+        var decision = context.decision;
+
+        // then
+        expect(decision).to.equal(decisionElement.businessObject);
+        expect(decision.literalExpression).to.exist;
+      });
+
+      // when
+      mouseEvent('click', drillDownOverlay);
+    }));
+
+
+    it('should have the drill-down overlay after deletion of element is undone', inject(function(elementRegistry, modeling, commandStack) {
+      // given
+      var decisionElement = elementRegistry.get('dish-decision'),
+          elements;
+
+      modeling.removeShape(decisionElement);
+
+      // when
+      commandStack.undo();
 
       // then
-      expect(decision).to.equal(decisionElement.businessObject);
-      expect(decision.literalExpression).to.exist;
-    });
+      elements = queryElement('.djs-overlays', true);
+      expect(elements).to.have.length(3);
+    }));
+  });
 
-    // when
-    mouseEvent('click', drillDownOverlay);
-  }));
+  describe('no navigation', function() {
+
+    beforeEach(bootstrapModeler(diagramXML, { modules: testModules, disableDrdInteraction: true }));
 
 
-  it('should have the drill-down overlay after deletion of element is undone', inject(function(elementRegistry, modeling, commandStack) {
-    // given
-    var decisionElement = elementRegistry.get('dish-decision'),
-        elements;
+    it('should not drill-down when navigation is disabled', inject(function(elementRegistry, eventBus) {
 
-    modeling.removeShape(decisionElement);
+      // given
+      var drillDownOverlay = queryElement('[data-container-id="dish-decision"] div div'),
+          drilledDown = false;
 
-    // when
-    commandStack.undo();
+      eventBus.on('decision.open', function(context) {
+        drilledDown = true;
+      });
 
-    // then
-    elements = queryElement('.djs-overlays', true);
-    expect(elements).to.have.length(3);
-  }));
+      // when
+      mouseEvent('click', drillDownOverlay);
+
+      // then
+      expect(drilledDown).to.be.false;
+    }));
+  });
+
+
 
 });
