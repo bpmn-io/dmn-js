9b57c62bf87b9441ecb97ee6ed7b9664778f186d
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-02-23 22:22:17 +0100

    chore(dmn-js): build and bundle pre-built distribution
    * builds pre-packaged distribution in dmn-js package.
    * use Webpack for bundling to properly handle ES6 / ES5 mix
    * add integration tests that verify the bundle works
    * hooks in bundle generation + testing via <prepublishOnly>
    
    Closes #302

---
+++ packages/dmn-js/tasks/test-distro.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var exec = require('execa').sync;
+
+var failures = 0;
+
+function runTest(variant, env) {
+
+  var NODE_ENV = process.env.NODE_ENV;
+
+  process.env.VARIANT = variant;
+  process.env.NODE_ENV = env;
+
+  console.log('[TEST] ' + variant + '@' + env);
+
+  try {
+    exec('karma', [ 'start', 'test/distro/karma.conf.js' ]);
+  } catch (e) {
+    console.error('[TEST] FAILURE');
+    console.error(e);
+
+    failures++;
+  } finally {
+    process.env.NODE_ENV = NODE_ENV;
+  }
+}
+
+function test() {
+
+  runTest('dmn-modeler', 'development');
+  runTest('dmn-modeler', 'production');
+
+  runTest('dmn-viewer', 'development');
+  runTest('dmn-viewer', 'production');
+
+  if (failures) {
+    process.exit(1);
+  }
+}
+
+
+test();
