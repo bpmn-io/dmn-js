7efbe4480a02b115083c973319acdb5a0bc817ed
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-02-25 21:53:43 +0100

    chore(distro): add license banner
    Prepend license notice to distro files.

--- packages/dmn-js/webpack.config.js
+++ packages/dmn-js/webpack.config.js
@@ -1,12 +1,33 @@
-var path = require('path');
+const path = require('path');
+const fs = require('fs');
 
-let minimize = false;
-let nameSuffix = 'development';
+const webpack = require('webpack');
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+const BannerPlugin = webpack.BannerPlugin;
+
+let minimize;
+let nameSuffix;
+let banner;
 
 if (process.env.NODE_ENV === 'production') {
   minimize = true;
   nameSuffix = 'production.min';
+  banner = fs.readFileSync(__dirname + '/resources/banner-min.txt', 'utf8');
+} else {
+  minimize = false;
+  nameSuffix = 'development';
+  banner = fs.readFileSync(__dirname + '/resources/banner.txt', 'utf8');
 }
+
+const version = require('./package').version;
+
+const bannerConfig = {
+  version,
+  date: today()
+};
+
 
 module.exports = {
   mode: 'production',
@@ -40,7 +61,56 @@ module.exports = {
       }
     ]
   },
+  plugins: [
+    new BannerPlugin({
+      banner: processTemplate(banner, bannerConfig),
+      raw: true
+    })
+  ],
   optimization: {
-    minimize
+    minimize,
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          output: {
+            comments: /license|@preserve/
+          }
+        }
+      })
+    ]
   }
 };
+
+
+//////////// helpers ///////////////////////
+
+function pad(n) {
+  if (n < 10) {
+    return '0' + n;
+  } else {
+    return n;
+  }
+}
+
+function today() {
+  const d = new Date();
+
+  return [
+    d.getFullYear(),
+    pad(d.getMonth() + 1),
+    pad(d.getDay())
+  ].join('-');
+}
+
+function processTemplate(str, args) {
+  return str.replace(/\{\{\s*([^\s]+)\s*\}\}/g, function(_, n) {
+
+    var replacement = args[n];
+
+    if (!replacement) {
+      throw new Error('unknown template {{ ' + n + '}}');
+    }
+
+    return replacement;
+  });
+}
