adaf2c5e2cae5fd1ec1d62ed06fc9da2fc4c50d0
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-04-26 14:53:00 +0200

    chore(drd): depend on overlays from drill-down module

--- packages/dmn-js-drd/lib/features/drill-down/index.js
+++ packages/dmn-js-drd/lib/features/drill-down/index.js
@@ -1,6 +1,11 @@
+import OverlaysModule from 'diagram-js/lib/features/overlays';
+
 import DrillDown from './DrillDown';
 
 export default {
+  __depends__: [
+    OverlaysModule
+  ],
   __init__: [ 'drillDown' ],
   drillDown: [ 'type', DrillDown ]
 };
