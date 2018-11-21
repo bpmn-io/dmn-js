4757559650e5b5f3391e5ad50df0ab2015b5aeb0
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2018-11-21 10:19:48 +0100

    chore(drd): inherit diagram-js keybindings
    * only bind keyboard bindings for which editor actions exist
    * rename DrdKeyBindings -> DrdKeyboardBindings to adapt diagram-js
    naming
    * add KeyEvents test util

---
+++ packages/dmn-js-drd/src/features/keyboard/DrdKeyboardBindings.js
@@ -0,0 +1,112 @@
+import inherits from 'inherits';
+
+import KeyboardBindings from 'diagram-js/lib/features/keyboard/KeyboardBindings';
+
+
+/**
+ * DRD specific key bindings.
+ *
+ * @param {Keyboard} keyboard
+ * @param {EditorActions} editorActions
+ */
+export default function DrdKeyboardBindings(injector) {
+  injector.invoke(KeyboardBindings, this);
+}
+
+inherits(DrdKeyboardBindings, KeyboardBindings);
+
+DrdKeyboardBindings.$inject = [
+  'injector'
+];
+
+
+/**
+ * Register available keyboard bindings.
+ *
+ * @param {Keyboard} keyboard
+ * @param {EditorActions} editorActions
+ */
+DrdKeyboardBindings.prototype.registerBindings = function(keyboard, editorActions) {
+
+  // inherit default bindings
+  KeyboardBindings.prototype.registerBindings.call(this, keyboard, editorActions);
+
+  /**
+   * Add keyboard binding if respective editor action
+   * is registered.
+   *
+   * @param {String} action name
+   * @param {Function} fn that implements the key binding
+   */
+  function addListener(action, fn) {
+
+    if (editorActions.isRegistered(action)) {
+      keyboard.addListener(fn);
+    }
+  }
+
+  // select all elements
+  // CTRL + A
+  addListener('selectElements', function(context) {
+
+    var event = context.keyEvent;
+
+    if (keyboard.isKey(['a', 'A'], event) && keyboard.isCmd(event)) {
+      editorActions.trigger('selectElements');
+
+      return true;
+    }
+  });
+
+  // activate space tool
+  // S
+  addListener('spaceTool', function(context) {
+
+    var event = context.keyEvent;
+
+    if (keyboard.hasModifier(event)) {
+      return;
+    }
+
+    if (keyboard.isKey(['s', 'S'], event)) {
+      editorActions.trigger('spaceTool');
+
+      return true;
+    }
+  });
+
+  // activate lasso tool
+  // L
+  addListener('lassoTool', function(context) {
+
+    var event = context.keyEvent;
+
+    if (keyboard.hasModifier(event)) {
+      return;
+    }
+
+    if (keyboard.isKey(['l', 'L'], event)) {
+      editorActions.trigger('lassoTool');
+
+      return true;
+    }
+  });
+
+  // activate direct editing
+  // E
+  addListener('directEditing', function(context) {
+
+    var event = context.keyEvent;
+
+    if (keyboard.hasModifier(event)) {
+      return;
+    }
+
+    if (keyboard.isKey(['e', 'E'], event)) {
+      editorActions.trigger('directEditing');
+
+      return true;
+    }
+  });
+
+};
---
+++ packages/dmn-js-drd/test/spec/features/keyboard/DrdKeyboardBindingsSpec.js
@@ -0,0 +1,131 @@
+import {
+  bootstrapViewer,
+  inject
+} from 'test/TestHelper';
+
+import {
+  forEach
+} from 'min-dash';
+
+import coreModule from 'src/core';
+import editorActionsModule from 'src/features/editor-actions';
+import labelEditingModule from 'src/features/label-editing';
+import lassoToolModule from 'diagram-js/lib/features/lasso-tool';
+import keyboardModule from 'src/features/keyboard';
+import modelingModule from 'src/features/modeling';
+
+import {
+  createKeyEvent
+} from 'test/util/KeyEvents';
+
+/* global sinon */
+
+
+describe('features - keyboard', function() {
+
+  var diagramXML = require('../../../fixtures/dmn/di.dmn');
+
+  beforeEach(bootstrapViewer(diagramXML, {
+    modules: [
+      coreModule,
+      labelEditingModule,
+      lassoToolModule,
+      keyboardModule,
+      editorActionsModule,
+      modelingModule
+    ]
+  }));
+
+
+  describe('drd keyboard bindings', function() {
+
+    it('should include triggers inside editorActions', inject(function(editorActions) {
+      // given
+      var expectedActions = [
+        'undo',
+        'redo',
+        'zoom',
+        'removeSelection',
+        'selectElements',
+        'lassoTool',
+        'directEditing'
+      ];
+
+      // then
+      expect(editorActions.getActions()).to.eql(expectedActions);
+    }));
+
+
+    forEach(['l', 'L'], function(key) {
+
+      it('should trigger lasso tool for key ' + key, inject(
+        function(keyboard, lassoTool) {
+
+          sinon.spy(lassoTool, 'activateSelection');
+
+          // given
+          var e = createKeyEvent(key);
+
+          // when
+          keyboard._keyHandler(e);
+
+          // then
+          expect(lassoTool.activateSelection).to.have.been.calledOnce;
+        }
+      ));
+
+    });
+
+
+    forEach(['e', 'E'], function(key) {
+
+      it('should trigger direct editing', inject(
+        function(keyboard, selection, elementRegistry, directEditing) {
+
+          sinon.spy(directEditing, 'activate');
+
+          // given
+          var task = elementRegistry.get('guestCount');
+
+          selection.select(task);
+
+          var e = createKeyEvent(key);
+
+          // when
+          keyboard._keyHandler(e);
+
+          // then
+          expect(directEditing.activate).to.have.been.calledOnce;
+        }
+      ));
+
+    });
+
+
+    forEach(['a', 'A'], function(key) {
+
+      it('should select all elements',
+        inject(function(canvas, keyboard, selection, elementRegistry) {
+
+          // given
+          var e = createKeyEvent(key, { ctrlKey: true });
+
+          var allElements = elementRegistry.getAll(),
+              rootElement = canvas.getRootElement();
+
+          // when
+          keyboard._keyHandler(e);
+
+          // then
+          var selectedElements = selection.get();
+
+          expect(selectedElements).to.have.length(allElements.length - 1);
+          expect(selectedElements).not.to.contain(rootElement);
+        })
+      );
+
+    });
+
+  });
+
+});
