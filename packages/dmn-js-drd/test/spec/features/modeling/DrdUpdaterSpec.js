85cbc89bfe9bd868d4423f3338b40dd83c6058ae
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-01-20 15:19:43 +0100

    feat(drd/drd-updater): migrate to DMN 1.3

--- packages/dmn-js-drd/test/spec/features/modeling/DrdUpdaterSpec.js
+++ packages/dmn-js-drd/test/spec/features/modeling/DrdUpdaterSpec.js
@@ -48,7 +48,7 @@ describe('features/modeling - DrdUpdater', function() {
 
   describe('update parent', function() {
 
-    it('should update parent', inject(
+    it('should update parent when decision is created', inject(
       function(elementFactory, elementRegistry, modeling) {
 
         // given
@@ -64,6 +64,315 @@ describe('features/modeling - DrdUpdater', function() {
         var decisionBo = decision.businessObject;
 
         expect(decisionBo.$parent).to.equal(definitionsBo);
+        expect(definitionsBo.drgElement).to.include(decisionBo);
+      }
+    ));
+
+
+    it('should update parent when decision is removed', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            definitionsBo = definitions.businessObject,
+            decision = elementRegistry.get('Decision_3'),
+            decisionBo = decision.businessObject;
+
+        // when
+        modeling.removeShape(decision);
+
+        // then
+        expect(decisionBo.$parent).to.be.null;
+        expect(definitionsBo.drgElement).to.not.include(decisionBo);
+      }
+    ));
+
+
+    it('should update parent when text annotation is created', inject(
+      function(elementFactory, elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            definitionsBo = definitions.businessObject;
+
+        var textAnnotation = elementFactory.create(
+          'shape', { type: 'dmn:TextAnnotation' }
+        );
+
+        // when
+        modeling.createShape(textAnnotation, { x: 100, y: 100 }, definitions);
+
+        // then
+        var textAnnotationBo = textAnnotation.businessObject;
+
+        expect(textAnnotationBo.$parent).to.eql(definitionsBo);
+        expect(definitionsBo.get('artifact')).to.include(textAnnotationBo);
+      }
+    ));
+
+
+    it('should update parent when text annotation is removed', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            definitionsBo = definitions.businessObject,
+            textAnnotation = elementRegistry.get('TextAnnotation_1');
+
+        // when
+        modeling.removeShape(textAnnotation);
+
+        // then
+        var textAnnotationBo = textAnnotation.businessObject;
+
+        expect(textAnnotationBo.$parent).to.be.null;
+        expect(definitionsBo.get('artifact')).to.not.include(textAnnotationBo);
+      }
+    ));
+
+
+    it('should update parent when information requirement is created', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var decision1 = elementRegistry.get('Decision_2'),
+            decision2 = elementRegistry.get('Decision_3');
+
+        // when
+        var informationRequirement = modeling.connect(decision1, decision2),
+            informationRequirementBo = informationRequirement.businessObject;
+
+        // then
+        expect(informationRequirementBo.$parent).to.eql(decision2.businessObject);
+      }
+    ));
+
+
+    it('should update parent when information requirement is removed', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var informationRequirement = elementRegistry.get('InformationRequirement_1'),
+            informationRequirementBo = informationRequirement.businessObject;
+
+        // when
+        modeling.removeConnection(informationRequirement);
+
+        // then
+        expect(informationRequirementBo.$parent).to.be.null;
+      }
+    ));
+
+
+    it('should update parent when association is created', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            definitionsBo = definitions.businessObject,
+            textAnnotation = elementRegistry.get('TextAnnotation_1'),
+            decision = elementRegistry.get('Decision_3');
+
+        // when
+        var association = modeling.connect(textAnnotation, decision),
+            associationBo = association.businessObject;
+
+        // then
+        expect(associationBo.$parent).to.eql(definitionsBo);
+        expect(definitionsBo.get('artifact')).to.include(associationBo);
+      }
+    ));
+
+
+    it('should update parent when association is removed', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            definitionsBo = definitions.businessObject,
+            association = elementRegistry.get('Association_1'),
+            associationBo = association.businessObject;
+
+        // when
+        modeling.removeConnection(association);
+
+        // then
+        expect(associationBo.$parent).to.be.null;
+        expect(definitionsBo.get('artifact')).to.not.include(associationBo);
+      }
+    ));
+
+  });
+
+
+  describe('update di parent', function() {
+
+    it('should update di parent when decision is created', inject(
+      function(elementFactory, elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            diagram = definitions.businessObject.dmnDI.diagrams[0];
+
+        var decision = elementFactory.create('shape', { type: 'dmn:Decision' });
+
+        // when
+        modeling.createShape(decision, { x: 100, y: 100 }, definitions);
+
+        // then
+        var di = decision.businessObject.di;
+
+        expect(di.$parent).to.equal(diagram);
+        expect(diagram.get('diagramElements')).to.include(di);
+      }
+    ));
+
+
+    it('should update di parent when decision is removed', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            decision = elementRegistry.get('Decision_3'),
+            diagram = definitions.businessObject.dmnDI.diagrams[0];
+
+
+        // when
+        modeling.removeShape(decision);
+
+        // then
+        var di = decision.businessObject.di;
+
+        expect(di.$parent).to.be.null;
+        expect(diagram.get('diagramElements')).to.not.include(di);
+      }
+    ));
+
+
+    it('should update di parent when text annotation is created', inject(
+      function(elementFactory, elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            definitionsBo = definitions.businessObject,
+            diagram = definitionsBo.dmnDI.diagrams[0];
+
+        var textAnnotation = elementFactory.create(
+          'shape', { type: 'dmn:TextAnnotation' }
+        );
+
+        // when
+        modeling.createShape(textAnnotation, { x: 100, y: 100 }, definitions);
+
+        // then
+        var di = textAnnotation.businessObject.di;
+
+        expect(di.$parent).to.eql(diagram);
+        expect(diagram.get('diagramElements')).to.include(di);
+      }
+    ));
+
+
+    it('should update di parent when text annotation is removed', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            definitionsBo = definitions.businessObject,
+            diagram = definitionsBo.dmnDI.diagrams[0],
+            textAnnotation = elementRegistry.get('TextAnnotation_1');
+
+        // when
+        modeling.removeShape(textAnnotation);
+
+        // then
+        var di = textAnnotation.businessObject.di;
+
+        expect(di.$parent).to.be.null;
+        expect(diagram.get('diagramElements')).to.not.include(di);
+      }
+    ));
+
+
+    it('should update di parent when information requirement is added', inject(
+      function(elementFactory, elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            diagram = definitions.businessObject.dmnDI.diagrams[0];
+
+        var decision = elementFactory.create('shape', { type: 'dmn:Decision' });
+
+        // when
+        modeling.createShape(decision, { x: 100, y: 100 }, definitions);
+
+        // then
+        var di = decision.businessObject.di;
+
+        expect(di.$parent).to.equal(diagram);
+        expect(diagram.get('diagramElements')).to.include(di);
+      }
+    ));
+
+
+    it('should update di parent when information requirement is removed', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            informationRequirement = elementRegistry.get('InformationRequirement_1'),
+            diagram = definitions.businessObject.dmnDI.diagrams[0];
+
+
+        // when
+        modeling.removeShape(informationRequirement);
+
+        // then
+        var di = informationRequirement.businessObject.di;
+
+        expect(di.$parent).to.be.null;
+        expect(diagram.get('diagramElements')).to.not.include(di);
+      }
+    ));
+
+
+    it('should update di parent when association is created', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            diagram = definitions.businessObject.dmnDI.diagrams[0],
+            textAnnotation = elementRegistry.get('TextAnnotation_1'),
+            decision = elementRegistry.get('Decision_3');
+
+        // when
+        var association = modeling.connect(textAnnotation, decision);
+
+        // then
+        var di = association.businessObject.di;
+
+        expect(di.$parent).to.eql(diagram);
+        expect(diagram.get('diagramElements')).to.include(di);
+      }
+    ));
+
+
+    it('should update di parent when association is removed', inject(
+      function(elementRegistry, modeling) {
+
+        // given
+        var definitions = elementRegistry.get('Definitions_1'),
+            diagram = definitions.businessObject.dmnDI.diagrams[0],
+            association = elementRegistry.get('Association_1');
+
+        // when
+        modeling.removeConnection(association);
+
+        // then
+        var di = association.businessObject.di;
+
+        expect(di.$parent).to.be.null;
+        expect(diagram.get('diagramElements')).to.not.include(di);
       }
     ));
 
