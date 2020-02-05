e0617e3d77d41c7534564d488457e4e0095d1eaf
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-02-05 17:15:52 +0100

    fix(drd/modeling): update connection `href` on source id change
    Related to https://github.com/camunda/camunda-modeler/issues/1679

--- packages/dmn-js-shared/src/features/modeling/behavior/IdChangeBehavior.js
+++ packages/dmn-js-shared/src/features/modeling/behavior/IdChangeBehavior.js
@@ -2,7 +2,10 @@ import { forEach } from 'min-dash';
 
 import CommandInterceptor from 'diagram-js/lib/command/CommandInterceptor';
 
-import { is } from '../../../util/ModelUtil';
+import {
+  is,
+  getBusinessObject
+} from '../../../util/ModelUtil';
 
 const ID = 'id';
 
@@ -17,11 +20,13 @@ export default class IdChangeBehavior extends CommandInterceptor {
   updateIds({ context }) {
     const { element, oldProperties, properties } = context;
 
-    if (!is(element, 'dmn:DRGElement') || !isIdChange(oldProperties, properties)) {
+    const bo = getBusinessObject(element);
+
+    if (!is(bo, 'dmn:DRGElement') || !isIdChange(oldProperties, properties)) {
       return;
     }
 
-    const drgElements = getDrgElements(element);
+    const drgElements = getDrgElements(bo);
 
     drgElements.forEach(drgElement => {
       updateElementReferences(drgElement, oldProperties.id, properties.id);
@@ -40,11 +45,27 @@ function isIdChange(oldProperties, properties) {
 }
 
 function getDrgElements(element) {
-  const definitions = element.$parent;
+  const definitions = getDefinitions(element);
 
   const drgElements = definitions.get('drgElement');
 
   return drgElements;
+}
+
+
+/**
+ * Walk up the tree until at the root to get to dmn:Definitions.
+ *
+ * @param {ModdleElement} element
+ */
+function getDefinitions(element) {
+  let definitions = element;
+
+  while (!is(definitions, 'dmn:Definitions')) {
+    definitions = definitions.$parent;
+  }
+
+  return definitions;
 }
 
 function updateElementReferences(element, oldId, id) {
