1eab60f341d662335b052e6ecd6439b1a6ba6c04
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2020-02-26 11:22:24 +0100

    test(decision-table): fix test race condition

--- packages/dmn-js-decision-table/test/spec/features/view-drd/ViewDrdSpec.js
+++ packages/dmn-js-decision-table/test/spec/features/view-drd/ViewDrdSpec.js
@@ -7,6 +7,7 @@ import { query as domQuery } from 'min-dom';
 
 import simpleXML from '../../simple.dmn';
 
+
 describe('view drd', function() {
 
   let testContainer;
@@ -20,29 +21,46 @@ describe('view drd', function() {
       container: testContainer
     });
 
-    viewer.importXML(xml, (err, warnings) => {
+    viewer.importXML(xml, { open: false }, (err, warnings) => {
+
+      const view = viewer._views.filter(v => v.type === 'decisionTable')[0];
+
+      expect(view).to.exist;
 
       // open decision table
-      viewer.open(viewer._views.filter(v => v.type === 'decisionTable')[0]);
-
-      done(err, warnings, viewer);
+      viewer.open(view, function(_err) {
+        return done(err || _err, warnings, viewer);
+      });
     });
   }
 
-  it('should not show view drd button', function() {
-    createViewer(DmnDecisionTableViewer, simpleXML, function() {
+
+  it('should not show view drd button', function(done) {
+    createViewer(DmnDecisionTableViewer, simpleXML, function(err) {
+
+      if (err) {
+        return done(err);
+      }
 
       // then
       expect(domQuery('.view-drd-button', testContainer)).to.not.exist;
+
+      return done();
     });
   });
 
 
-  it('should show view drd button', function() {
-    createViewer(MockViewer, simpleXML, function() {
+  it('should show view drd button', function(done) {
+    createViewer(MockViewer, simpleXML, function(err) {
+
+      if (err) {
+        return done(err);
+      }
 
       // then
       expect(domQuery('.view-drd-button', testContainer)).to.exist;
+
+      return done();
     });
   });
 
