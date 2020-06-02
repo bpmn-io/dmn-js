7b3e2f0d6f07d4f9b33c56ebf7d0b623cdd04314
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-06-02 19:20:45 +0200

    feat(decision-table/context-menu): move cell entries to the bottom

--- packages/dmn-js-decision-table/src/features/context-menu/ContextMenu.js
+++ packages/dmn-js-decision-table/src/features/context-menu/ContextMenu.js
@@ -47,7 +47,7 @@ export default class ContextMenu {
 
           return () => (
             <div className="context-menu-flex">
-              { [ cellEntriesGroup ].concat(entries || []) }
+              { (entries || []).concat(cellEntriesGroup) }
             </div>
           );
         } else {
