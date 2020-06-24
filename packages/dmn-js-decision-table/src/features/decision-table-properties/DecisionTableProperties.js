5734b4930dd6814800bf882a508f609633811315
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-06-24 09:28:40 +0200

    feat(decision-table/head): move decision properties above table

--- packages/dmn-js-decision-table/src/features/decision-table-properties/DecisionTableProperties.js
+++ packages/dmn-js-decision-table/src/features/decision-table-properties/DecisionTableProperties.js
@@ -1,8 +1,10 @@
 import Component from './components/DecisionTablePropertiesComponent';
 
+const LOW_PRIORITY = 500;
+
 export default class DecisionTableProperties {
   constructor(components) {
-    components.onGetComponent('table.header', () => {
+    components.onGetComponent('table.before', LOW_PRIORITY, () => {
       return Component;
     });
   }
