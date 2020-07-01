84c1a6c552c4aee39ba5de92fde8bf3c3f5443a4
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-07-01 14:11:00 +0200

    feat(decision-table/annotations): allow to resize
    Closes #500

--- packages/dmn-js-decision-table/src/features/annotations/components/AnnotationHeader.js
+++ packages/dmn-js-decision-table/src/features/annotations/components/AnnotationHeader.js
@@ -1,9 +1,68 @@
-export default function AnnotationHeader(props, context) {
-  const _translate = context.injector.get('translate');
+import { Component } from 'inferno';
 
-  return (
-    <th className="annotation header">
-      { _translate('Annotations') }
-    </th>
-  );
+import {
+  inject,
+  mixin
+} from 'table-js/lib/components';
+
+import {
+  ComponentWithSlots
+} from 'dmn-js-shared/lib/components/mixins';
+
+
+const MIN_WIDTH = 400;
+
+
+export default class AnnotationHeader extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    mixin(this, ComponentWithSlots);
+
+    inject(this);
+  }
+
+  componentDidMount() {
+    this.changeSupport.onElementsChanged(this.getRoot(), this.onElementsChanged);
+  }
+
+  componentWillUnmount() {
+    this.changeSupport.offElementsChanged(this.getRoot(), this.onElementsChanged);
+  }
+
+  onElementsChanged = () => {
+    this.forceUpdate();
+  }
+
+  getRoot() {
+    return this.sheet.getRoot();
+  }
+
+  render() {
+    const decisionTable = this.getRoot();
+    const annotationsWidth = decisionTable.businessObject.get('annotationsWidth');
+    const width = (annotationsWidth || MIN_WIDTH) + 'px';
+
+    return (
+      <th
+        className="annotation header"
+        style={ { width } }
+      >
+        {
+          this.slotFills({
+            type: 'cell-inner',
+            context: {
+              cellType: 'annotations',
+              col: this.sheet.getRoot(),
+              minWidth: MIN_WIDTH
+            }
+          })
+        }
+
+        { this.translate('Annotations') }
+      </th>
+    );
+  }
 }
+
+AnnotationHeader.$inject = [ 'changeSupport', 'sheet', 'translate' ];
