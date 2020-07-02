6e13f18c41c5a79ba11e3d854c8a2f6978506db6
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-07-02 16:33:13 +0200

    fix(decision-table): correct title for add output button

--- packages/dmn-js-decision-table/src/features/add-input-output/components/AddOutput.js
+++ packages/dmn-js-decision-table/src/features/add-input-output/components/AddOutput.js
@@ -48,7 +48,7 @@ export default class AddOutput extends Component {
       <div
         onClick={ this.handleClick }
         className="add-output actionable"
-        title={ this._translate('Add Input') }
+        title={ this._translate('Add Output') }
       >
         <span
           className="dmn-icon-plus action-icon"
