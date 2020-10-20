e17931fbc681c2921390522bbc8029e5a94f554a
Alexis Zeghers <42282939+AZeghers@users.noreply.github.com>
2020-10-20 14:08:35 +0200

    feat(add-row): add rule focusing behavior
    -on clicking the add rule button, focus the first cell of the new row
    -on clicking the last row, focus the clicked cell of the new row
    
    Related to https://github.com/camunda/camunda-modeler/issues/928

--- packages/dmn-js-decision-table/src/features/add-rule/AddRule.js
+++ packages/dmn-js-decision-table/src/features/add-rule/AddRule.js
@@ -1,13 +1,20 @@
 import AddRuleFootComponent from './components/AddRuleFootComponent';
 
 export default class AddRule {
-  constructor(components, editorActions, eventBus) {
+  constructor(components, editorActions, eventBus, selection) {
     components.onGetComponent('table.foot', () => AddRuleFootComponent);
 
-    eventBus.on('addRule', () => {
-      editorActions.trigger('addRule');
+    eventBus.on('addRule', (e, context) => {
+      const rule = editorActions.trigger('addRule');
+      const colIndex = context.colIndex;
+
+      if (rule.cells[colIndex]) {
+        selection.select(rule.cells[colIndex]);
+      } else {
+        selection.select(rule.cells[0]);
+      }
     });
   }
 }
 
-AddRule.$inject = [ 'components', 'editorActions', 'eventBus' ];
+AddRule.$inject = [ 'components', 'editorActions', 'eventBus', 'selection' ];
