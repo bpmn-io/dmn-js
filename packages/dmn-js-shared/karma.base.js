3d736572c3880c873190d8b7a86886cbe2a00edd
Max Tru <maximilian.trumpf@camunda.com>
2021-06-14 12:49:19 +0200

    chore: run tests on modern browser environment
    * this allows us to use ES6 constructs such as async within the test
      suite. It will not impact the dist code.

--- packages/dmn-js-shared/karma.base.js
+++ packages/dmn-js-shared/karma.base.js
@@ -56,7 +56,19 @@ module.exports = function(path) {
             {
               test: /\.js$/,
               exclude: /node_modules/,
-              use: 'babel-loader'
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  presets: [
+                    ['@babel/preset-env', {
+                      modules: false,
+                      targets: {
+                        chrome: '91'
+                      }
+                    }]
+                  ]
+                }
+              }
             },
             {
               test: /\.css|\.dmn$/,
