659620694c295abafef4fdbe099c5cac9d9d78cd
Philipp <philippfromme@outlook.com>
2021-10-18 10:35:21 +0200

    chore(dmn-js-drd): internalize updating UI

--- packages/dmn-js-drd/src/features/definition-properties/modeler.js
+++ packages/dmn-js-drd/src/features/definition-properties/modeler.js
@@ -1,7 +1,9 @@
+import DefinitionPropertiesView from './viewer';
 import DefinitionPropertiesEdit from './DefinitionPropertiesEdit';
 import PaletteAdapter from './PaletteAdapter';
 
 export default {
+  __depends__: [ DefinitionPropertiesView ],
   __init__: [
     'definitionPropertiesEdit',
     'definitionPropertiesPaletteAdapter'
