dc5a17f3eabecea369de07da47483802e17bc234
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2022-08-17 11:33:45 +0200

    chore: generate coverage reports with HTML

--- packages/dmn-js-shared/karma.base.js
+++ packages/dmn-js-shared/karma.base.js
@@ -39,7 +39,7 @@ module.exports = function(path) {
 
       coverageReporter: {
         reporters: [
-          { type: 'lcovonly', subdir: '.' },
+          { type: 'lcov', subdir: '.' },
         ]
       },
 
