53dd06fd7bc196eab027d4affcbf646f4954d7f0
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2024-03-25 17:09:46 +0100

    chore: refactor element variable

--- packages/dmn-js-boxed-expression/src/features/element-variable/editor.js
+++ packages/dmn-js-boxed-expression/src/features/element-variable/editor.js
@@ -1,6 +1,8 @@
 import ElementVariableEditor from './ElementVariableEditor';
+import ElementVariableEditorComponent from './components/ElementVariableEditorComponent';
 
 export default {
-  __init__: [ 'elementVariable' ],
-  elementVariable: [ 'type', ElementVariableEditor ]
+  __init__: [ 'elementVariable', 'elementVariableComponent' ],
+  elementVariable: [ 'type', ElementVariableEditor ],
+  elementVariableComponent: [ 'type', ElementVariableEditorComponent ]
 };
