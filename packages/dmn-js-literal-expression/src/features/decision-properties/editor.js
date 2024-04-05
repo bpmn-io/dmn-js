9b9d1a7a0b6fdd723d0d13be7cf1b91944c3ebd1
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2024-04-05 10:30:42 +0200

    fix: ensure accessible names for inputs
    Closes #821

--- packages/dmn-js-literal-expression/src/features/decision-properties/editor.js
+++ packages/dmn-js-literal-expression/src/features/decision-properties/editor.js
@@ -1,8 +1,10 @@
+import TranslateModule from 'diagram-js/lib/i18n/translate';
+
 import DecisionPropertiesEditor from './DecisionPropertiesEditor';
 import DebounceInput from 'dmn-js-shared/lib/features/debounce-input';
 
 export default {
-  __depends__: [ DebounceInput ],
+  __depends__: [ DebounceInput, TranslateModule ],
   __init__: [ 'decisionProperties' ],
   decisionProperties: [ 'type', DecisionPropertiesEditor ]
 };
