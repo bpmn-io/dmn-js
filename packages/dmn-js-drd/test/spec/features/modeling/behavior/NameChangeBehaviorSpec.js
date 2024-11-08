38b5c3177e10b870e632a39994f6d0ff32053ec8
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2024-11-08 10:18:14 +0100

    fix: make name change behavior not break on name change
    Related to https://github.com/bpmn-io/dmn-js-properties-panel/issues/103
    Related to https://github.com/camunda/camunda-modeler/issues/4684

--- packages/dmn-js-drd/test/spec/features/modeling/behavior/NameChangeBehaviorSpec.js
+++ packages/dmn-js-drd/test/spec/features/modeling/behavior/NameChangeBehaviorSpec.js
@@ -8,66 +8,117 @@ import Modeling from 'src/features/modeling';
 
 describe('NameChangeBehavior', function() {
 
-  describe('with label change', function() {
-
-    beforeEach(bootstrapModeler(simpleStringEditXML, {
-      modules: [
-        CoreModule,
-        Modeling
-      ],
-    }));
-
-    describe('should update variable name when label is changed', function() {
+  beforeEach(bootstrapModeler(simpleStringEditXML, {
+    modules: [
+      CoreModule,
+      Modeling
+    ],
+  }));
 
 
-      it('<do>', inject(
-        function(modeling, elementRegistry) {
+  describe('should update variable name when label is changed', function() {
 
-          // given
-          const decision = elementRegistry.get('season'),
-                bo = decision.businessObject,
-                variable = bo.variable;
-
-          // when
-          modeling.updateLabel(decision,'foo');
-
-          // then
-          expect(variable.get('name')).to.equal('foo');
-        }
-      ));
-
-
-      it('<undo>', inject(function(modeling, elementRegistry, commandStack) {
+    it('<do>', inject(
+      function(modeling, elementRegistry) {
 
         // given
         const decision = elementRegistry.get('season'),
               bo = decision.businessObject,
               variable = bo.variable;
-        modeling.updateLabel(decision,'foo');
 
         // when
-        commandStack.undo();
-
-        // then
-        expect(variable.get('name')).to.equal('season');
-      }));
-
-
-      it('<redo>', inject(function(modeling, elementRegistry, commandStack) {
-
-        // given
-        const decision = elementRegistry.get('season'),
-              bo = decision.businessObject,
-              variable = bo.variable;
         modeling.updateLabel(decision,'foo');
-
-        // when
-        commandStack.undo();
-        commandStack.redo();
 
         // then
         expect(variable.get('name')).to.equal('foo');
-      }));
-    });
+      }
+    ));
+
+
+    it('<undo>', inject(function(modeling, elementRegistry, commandStack) {
+
+      // given
+      const decision = elementRegistry.get('season'),
+            bo = decision.businessObject,
+            variable = bo.variable;
+      modeling.updateLabel(decision,'foo');
+
+      // when
+      commandStack.undo();
+
+      // then
+      expect(variable.get('name')).to.equal('season');
+    }));
+
+
+    it('<redo>', inject(function(modeling, elementRegistry, commandStack) {
+
+      // given
+      const decision = elementRegistry.get('season'),
+            bo = decision.businessObject,
+            variable = bo.variable;
+      modeling.updateLabel(decision,'foo');
+
+      // when
+      commandStack.undo();
+      commandStack.redo();
+
+      // then
+      expect(variable.get('name')).to.equal('foo');
+    }));
+  });
+
+
+  describe('should update variable name when element name is changed', function() {
+
+
+    it('<do>', inject(
+      function(modeling, elementRegistry) {
+
+        // given
+        const decision = elementRegistry.get('season'),
+              bo = decision.businessObject,
+              variable = bo.variable;
+
+        // when
+        modeling.updateProperties(decision, { name: 'foo' });
+
+        // then
+        expect(variable.get('name')).to.equal('foo');
+      }
+    ));
+
+
+    it('<undo>', inject(function(modeling, elementRegistry, commandStack) {
+
+      // given
+      const decision = elementRegistry.get('season'),
+            bo = decision.businessObject,
+            variable = bo.variable;
+      modeling.updateProperties(decision, { name: 'foo' });
+
+      // when
+      commandStack.undo();
+
+      // then
+      expect(variable.get('name')).to.equal('season');
+    }));
+
+
+    it('<redo>', inject(function(modeling, elementRegistry, commandStack) {
+
+      // given
+      const decision = elementRegistry.get('season'),
+            bo = decision.businessObject,
+            variable = bo.variable;
+      modeling.updateProperties(decision, { name: 'foo' });
+
+      // when
+      commandStack.undo();
+      commandStack.redo();
+
+      // then
+      expect(variable.get('name')).to.equal('foo');
+    }));
   });
 });
