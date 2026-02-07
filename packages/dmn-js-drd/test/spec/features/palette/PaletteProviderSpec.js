a1ca5597a307644fe6bf3e5c8b5ee484d08d7103
Abiola <farounbiabiola@gmail.com>
2026-02-07 21:55:27 +0000

    remove unwanted changes

--- packages/dmn-js-drd/test/spec/features/palette/PaletteProviderSpec.js
+++ packages/dmn-js-drd/test/spec/features/palette/PaletteProviderSpec.js
@@ -30,7 +30,7 @@ describe('features/palette', function() {
 
     // when
     var paletteElement = domQuery('.djs-palette', canvas._container);
-    var entries = domQueryAll('.entry', paletteElement)
+    var entries = domQueryAll('.entry', paletteElement);
 
     // then
     expect(entries.length).to.equal(7);
