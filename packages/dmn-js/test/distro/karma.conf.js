a67ed9f848f3caba57d8b05637aeadbdec8d694e
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-05-05 14:19:37 +0200

    chore: remove legacy CI hack

--- packages/dmn-js/test/distro/karma.conf.js
+++ packages/dmn-js/test/distro/karma.conf.js
@@ -1,21 +1,6 @@
 // configures browsers to run test against
-// any of [ 'ChromeHeadless', 'Chrome', 'Firefox', 'IE' ]
-var browsers =
-  (process.env.TEST_BROWSERS || 'ChromeHeadless')
-    .replace(/^\s+|\s+$/, '')
-    .split(/\s*,\s*/g)
-    .map(function(browser) {
-      if (browser === 'ChromeHeadless') {
-        process.env.CHROME_BIN = require('puppeteer').executablePath();
-
-        // workaround https://github.com/GoogleChrome/puppeteer/issues/290
-        if (process.platform === 'linux') {
-          return 'ChromeHeadless_Linux';
-        }
-      }
-
-      return browser;
-    });
+// any of [ 'ChromeHeadless', 'Chrome', 'Firefox' ]
+var browsers = (process.env.TEST_BROWSERS || 'ChromeHeadless').split(',');
 
 
 var VARIANT = process.env.VARIANT;
